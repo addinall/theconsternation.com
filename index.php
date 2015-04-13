1ec0a82aff8efb02d0e477056ac4e8a7b856e62d
addinall <addinall@addinall.net>
2015-04-14 00:59:50 +1000

    First push

---
+++ index.php
@@ -0,0 +1,119 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+<!-- vim: set expandtab tabstop=4 shiftwidth=4 autoindent smartindent:
+//---------------------------------------------------------
+// CAPTAIN  SLOG
+//---------------------------------------------------------
+//
+//	FILE:       index.php 
+//	SYSTEM:     The Consternation landing page
+//	AUTHOR:     Mark Addinall
+//	DATE:       12th April 2015
+//	SYNOPSIS:   I was supposed to write this for the gang and have it 
+//	            published by April Fools Day.  I missed that deadline due
+//              to a bunch of morons who will remain nameless.
+//              Anyway, Sunday lunch time.  I'll spend a good solid day on this.
+//              Published 0300 13th April.  Not bad from scratch!  ;-)
+-->
+
+        <title>Yet Another Consternation</title>
+        <meta name="description" content="Responsive CSS3 - HTML5 demonstration">
+        <meta name="author" content="Mark Addinall Brisbane Australia">
+
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+
+<!--    This is just taken from my simple boilerplate HTML5 construction set.
+        Nothing fancy, just clean HTML5.  The CSS tags on the HTML elements are similary
+        simple to understand and use. -->
+
+    <body>
+        <div class="sheet">
+
+        <!-- this is our outer "wrapper" if you like.  It presents a page of the newspaper -->
+
+            <h1>The Consternation</h1>
+            <h4>diligentia maximum etiam mediocris ingeni subsidium </h4>
+            <hr>
+            <div class="news">
+
+            <!-- The newspaper of course has news, as opposed to headers, footers, menus
+                 and any other bits-o-fluff you want to bolt on.  It is semantically plain -->
+
+                <article>
+                    <p class="lead">
+Iram pande mihi Pelidae, Diua, superbi Tristia quae miseris iniecit funera Grais Atque animas fortes heroum tradidit 
+                    </p>
+                    <p>
+                    Orco Latrantumque dedit rostris uolucrumque trahendos Illorum exsangues, inhumatis ossibus, artus.  Confiebat enim summi sententia regis, protulerant* ex quo discordia pectora pugnas, Sceptriger Atrides et bello clarus Achilles.Quis deus hos ira tristi contendere iussit?  Latonae et magni proles Iouis. Ille Pelasgum infestam regi pestem in praecordia misit implicuitque graui Danaorum corpora morbo.  Nam quondam Chryses, sollemni tempora uitta implicitus, raptae fleuit solacia natae inuisosque dies inuisaque tempora noctis egit et assiduis impleuit questibus auras.
+                    </p>
+                    <p>
+Postquam nulla dies animum maerore leuabat nullaque lenibant patrios solacia fletus, castra petit Danaum genibusque affusus Atridae per superos regnique decus miserabilis orat, ut sibi causa suae reddatur nata salutis.  Dona simul praefert. Vincuntur fletibus eius Myrmidones reddique patri <img src="images/helen.jpg" alt="A picure of Helen of Troy on a stake."> 
+Chryseida censent.  Sed negat Atrides Chrysenque excedere castris despecta pietate iubet: ferus ossibus imis haeret amor spernitque preces damnosa libido.
+                    </p>
+                    <p>
+                     Contemptus repetit Phoebeia templa sacerdos squalidaque infestis maerens secat unguibus ora dilaceratque comas annosaque tempora plangit.
+                    </p>
+                    <p>
+                    Instead of Lorem Ipsum, I thought it would be nice to get some Homer and the Iliad.  However,  Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.  So we should not thumb our noses at this ubiquitous text of the typesetter's trade!
+                </article>
+                <hr>
+                <!-- MA Stuff about me! -->
+                <article>
+                    <p class="lead">
+                    I QUIT!<img src="images/me.jpg" alt="Picture of a terrifying historical villian.">
+ Hence Marcus has had a few days to sort out the Website of Lawrence Ridolfi, and make a start on this little bit-o-fun!
+                    </p>
+                    <p>
+                    Marcus was being made to do <em>dumb</em> things, and we all know that Marcus hates doing <em>dumb</em> things.  On the front end, what was called design was to have a front page that consited of a slider and six boxes.  Every new customer got different colour boxes!  I didn't mind that so much, apart from the fact that all the oddly coloured boxes were stuffed into tables.  Not new fancy CSS3 stuff, the old way with TR and TD and other horrors from the past.  However, I was getting along, then nek minnit....   Something bizarre happened.  I got taken out of the CSS club, and was made King of the Bug Ticket Queue.                    
+                    </p>
+                    <p>
+                    Marcus was wary of this turnup, especially as I seemed to be the last one notified.  "What the hell, give it a go".  But when one goes a few months without even picking up the new Fender, cancels Guitar lessons, puts all university studies on the never..never, gives up reading books, and surfing, and riding because work is so goddamn awful and you have a migrane <em>nearly</em> every day, then it is time to cut losses and walk away.
+                    </p>
+                    <p>
+                    So once again, shopping for a contract.  NO Drupal.  No JOOMLA! or other messy piles of doo-doo coded by a handfull of frustrated script-kiddies.  Sensible offers only please.  Have a look at some of my work <a href= "http://www.addinall.net" target="_blank">here</a> and also  somewhere around <a href= "http://www.addinall.org" target="_blank">here</a>.  
+                    </p>
+                    <p>
+                    Tools offered should include HTML5/CSS3/Modern PHP/mySQL (one that will run faster than nine transactions a second would be a plus).  Stuff like that.  View the source on this little landing page and have a look at my style by all means. The CSS is supposed to look like a paper on a desktop with the sun shining through a window.  That was my idea of the colours anyhow.
+                    </p>
+                </article>
+                <hr>
+                <article>
+                    <p class=lead>
+                    In the Beginning...... The Consternation was dreamed up by a mate of mine, Bernd Felsche, as a play on the electronic publication 'The Conversation'.
+                    </p>
+                    <p>
+                    So a few of we oldies joined in.  Initially with the brief to form a secret group to have <em>"Sikrut discussions about The Consternation web site including stuff about (dis)content management".</em> With the goal to publish a paper to take the piss out of silly silly people <img src="images/yf.jpg" alt="Mad Scientist"> who think the wurld is going to burst into flames because we drive a car, or eat cows (or indeed sneaky people who don't actually believe this, but would rather you lot did, and send money).  I volunteered to publish the paper however the aforementioned horrors made me miss my deadline.
+                    </p>
+                    <p>
+                    Bernd did a might fine job on his own creating The Consternation, which can be viewed <a href="http://www.theconsternation.net" target="_blank">here</a>.
+                    </p>
+                </article>
+                <hr>
+                <article>
+                    <p class=lead>
+                    The Science is in.  It is settled.  Vampires cause Tuberculosis.  This well known scientific fact lasted until the mid 1870s in the USA.
+                    </p>
+                    <p>
+                    The connection between vampires and tuberculosis, or “consumption” as it was then known, originated in Eastern Europe but was especially common in 19th century New England. When people died from consumption and other members of their household soon sickened as well, it was believed that the dead family member was visiting the house at night as a vampire <img src="images/vampire.jpg" alt="Wampire"> and slowly sucking their lives away. Because tuberculosis patients were slowly being “consumed” by the disease, it was assumed that vampires were feeding off of their blood.
+                    </p>
+                    <p>
+If multiple family members began to fall ill after a consumption death and vampirism was expected, communities took a nontraditional approach to treating the disease, beginning with exhuming the body and removing its heart. If the heart contained “fresh” blood (an effect of decomposition, but believed to be blood the vampire had sucked out of the living), the heart would be burned and the ashes fed to sick family members as a cure, or a stake would be driven through the heart, a practice still associated with vampire myths today.
+                    </p>
+                    <p>
+Symptoms of tuberculosis and the appearance of a decomposing body also contributed to the connection between the illness and the vampire myth. Common tuberculosis symptoms, including red, swollen, light-sensitive eyes, pale skin, low body heat, a weak heartbeat, and coughing up blood, as well the breakdown of lung tissue during and after death from tuberculosis, which caused the lips of corpses to appear bloody, all aligned with common beliefs about vampires. Taken from AERAS.
+                    </p>
+                    <p>
+                    This reminds me of the current religeous panic being spread by the Church of Global Warming.  How a 'raise' in the 'global' temperature will surely 'cause' disease, pestilence, zombies, mutant giant spiders, yada yada yada.  Same deal.  Mob mentality.  DENIER!!!
+                    </p>
+                </article>
+                <hr>
+            </div>
+            <p class = footer>
+            &copy; MMXV - Marcus the Great of Brisbane.  <a href="mailto:"addinall@addinall.net>Request an Audience</a>
+            </p>
+        </div>
+    </body>
+</html>
